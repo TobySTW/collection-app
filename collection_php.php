872310840b554984cc4dd8fbfa22c63a9353111c
Toby Williams <toby.williams@designatron.com>
2021-03-30 14:19:51 +0100

    Added HTML form

--- collection_php.php
+++ collection_php.php
@@ -1,3 +1,20 @@
+<h1>OverHyped Gaming</h1>
+<form>
+    <pre>
+        <h3>Title</h3>
+        <input type="text" name="form_title" placeholder="new title" maxlength="20">
+    </pre>
+    <pre>
+        <h3>Date</h3>
+        <input type="date" name="form_release" placeholder="date">
+    </pre>
+    <pre>
+        <h3>Description</h3>
+        <input type="text" name="form_reason" placeholder="description" maxlength="600">
+    </pre>
+    <input type="submit">
+</form>
+
 <?php
 $db = new PDO('mysql:host=db; dbname=collectionDB', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
