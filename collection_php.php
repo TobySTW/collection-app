35806be31bafa39948217a0f6e4095036040a664
Toby Williams <toby.williams@designatron.com>
2021-03-31 11:21:10 +0100

    reordered code to update page without refreshing and minor syntax patch

--- collection_php.php
+++ collection_php.php
@@ -9,38 +9,6 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 // This line flags an error with the code if something isn't connecting
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Here we prepare the query, ready to run it
-$query = $db->prepare('SELECT * FROM `collection_table`;');
-
-// We actually run the query against the DB
-$query->execute();
-
-// Returns the full array from the database
-$result = $query->fetchAll();
-
-?>
-
-<!-- This form collects data to be uploaded to the database -->
-<h1>OverHyped Gaming</h1>
-<form method="post">
-    <pre>
-        <h3>Title</h3>
-        <input type="text" name="form_title" placeholder="new title" maxlength="20">
-    </pre>
-    <pre>
-        <h3>Date</h3>
-        <input type="date" name="form_release" placeholder="date">
-    </pre>
-    <pre>
-        <h3>Description</h3>
-        <input type="text" name="form_reason" placeholder="description" maxlength="600">
-    </pre>
-    <input type="submit">
-</form>
-
-
-<?php
-
 // Checks the fields have all been filled out
 if (isset($_POST['form_title']) && ($_POST['form_release']) && ($_POST['form_reason'])) {
 
@@ -60,6 +28,38 @@ if (isset($_POST['form_title']) && ($_POST['form_release']) && ($_POST['form_rea
     // Executing the INSERT INTO data too the db
     $query->execute();
 }
+
+// Here we prepare the query, ready to run it
+$query = $db->prepare('SELECT * FROM `collection_table`;');
+
+// We actually run the query against the DB
+$query->execute();
+
+// Returns the full array from the database
+$result = $query->fetchAll();
+
+?>
+
+<!-- This form collects data to be uploaded to the database -->
+<h1>OverHyped Gaming</h1>
+<form method="post">
+    <div>
+        <h3>Title</h3>
+        <input type="text" name="form_title" placeholder="new title" maxlength="20">
+    </div>
+    <div>
+        <h3>Date</h3>
+        <input type="date" name="form_release" placeholder="date">
+    </div>
+    <div>
+        <h3>Description</h3>
+        <input type="text" name="form_reason" placeholder="description" maxlength="600">
+    </div>
+    <input type="submit">
+</form>
+
+
+<?php
 
 // applying a set of instructions for each row in the $result array
 foreach ($result as $item) {
